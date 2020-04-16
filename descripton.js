5319cae9bfbc78e52bdc350ea294300bc005f75e
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-04-15 20:13:29 -0400

    wip

---
+++ descripton.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Description (){
+    import React, {useState, useEffect } from "react";
+
+}
+
+export default Description
--- src/App.js
+++ src/App.js
@@ -1,14 +1,68 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+
+// function app(){
+  //set your states const [xyz,setxyz] = useState()
+
+  //set your useEfects.. axios.get("").then(response => { setNasaDailyData(response.data)}) .catch(error {console.log(error)})
+
+  // use your RETURNS classes classNames ontop of classes with names putting your images in.
+//}
+
+
 
 function App() {
+
+  //VARIABLES/ States AND URL
+  const [nasaData, setNasaData] = useState(null);
+
+  const url = "https://api.nasa.gov/planetary/apod?api_key=PD7NNnG6RckiIxO3JjBI0GqAGLqcPhl4izVHEfMP";
+
+  
+//USE EFFECT
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setNasaData(response.data)
+      console.log(response.data)
+      const data = (response.data)
+      console.log(data.title)
+    })
+  },[])
+
+
+  //Return Data in App Funtion
   return (
+    // const data = (response.data)
+
     <div className="App">
+
+    <div className="Title">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        {/* {nasaData && {data.title}} */}
       </p>
     </div>
+
+    <div className ="ImageTitle">
+      
+    </div>
+
+    <div className = "Description">
+
+    {nasaData &&<p>{nasaData.explanation}</p>}
+
+    </div>
+
+    <div className = "Image">
+
+
+    </div>
+
+    <div className = "Nav">
+
+    </div>
+    </div>
+    
   );
 }
 
